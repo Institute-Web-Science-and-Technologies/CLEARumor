b794414be2ae328dc315cb6d3980effc6eb76a2f
isspek <ipekbrs@gmail.com>
2019-09-30 23:03:18 +0200

    Revert "changed setup"
    This reverts commit 6b053c71ee75dd4486dccad562770fa9e7e6afb9.

--- setup.py
+++ setup.py
@@ -1,24 +1,13 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
-    name='clearumor',
-    version='1.0.1',
+    name='CLEARumor',
+    version='1.0.0',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     url='https://github.com/Institute-Web-Science-and-Technologies/CLEARumor.git',
-    license='Apache License 2.0',
+    license='',
+    author='',
+    author_email='',
     description='CLEARumor implementation',
     classifiers = [
         # How mature is this project? Common values are
@@ -26,6 +15,5 @@ setup(
         #   4 - Beta
         #   5 - Production/Stable
         'Development Status :: 3 - Alpha'],
-    dependency_links=['https://github.com/erikavaris/tokenizer.git'],
-    include_package_data=True
+    dependency_links=['https://github.com/erikavaris/tokenizer.git']
 )
