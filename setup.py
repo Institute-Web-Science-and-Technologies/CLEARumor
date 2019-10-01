101cdb64e0879c7c2f443588437a37518cd5a8bc
isspek <ipekbrs@gmail.com>
2019-10-01 22:57:01 +0200

    changed version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='clearumor',
-    version='1.0.1',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    url='https://github.com/Institute-Web-Science-and-Technologies/CLEARumor.git',
+    version='1.0.2',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests'],include=['src','src.*']),
+    url='https://github.com/Institute-Web-Science-and-Technologies/CLEARumor',
     description='CLEARumor implementation',
     license = 'Apache License 2.0',
     classifiers = [
@@ -14,5 +14,5 @@ setup(
         #   5 - Production/Stable
         'Development Status :: 3 - Alpha'],
     dependency_links=['https://github.com/erikavaris/tokenizer.git'],
-    package_data = {'src':['*']}
+    python_requires='>=3.6'
 )
